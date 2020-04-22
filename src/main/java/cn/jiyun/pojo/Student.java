7bfb5a19d2e091b3c51cf1ee5d8b716ddf30f697
jiucheng1997 <1453260083@qq.com>
2020-04-22 09:50:08 +0800

    刘世辉11122

---
+++ src/main/java/cn/jiyun/pojo/Student.java
@@ -0,0 +1,40 @@
+package cn.jiyun.pojo;
+
+import java.util.Date;
+
+public class Student {
+
+    private String userName;
+    private Integer id;
+    private String address;
+    private String hobby;
+    private Integer age;
+    private Date beginDate;
+    private  String  uname;
+
+
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
